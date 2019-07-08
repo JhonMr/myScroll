321f2c0fdfc5df697e27b8335a06f6b67df5c25f
jhonMr <729505179@qq.com>
2019-07-08 12:55:05 +0800

    force and limit move

--- src/myScroll.js
+++ src/myScroll.js
@@ -4,7 +4,7 @@
 	Description: js模拟滑动库
  */
 
-var throttle = function(fn, delay=50) {
+var throttle = function(fn, delay=20) {
 	var timeout = null;
 	return function() {
 		if(!timeout) {
@@ -20,23 +20,30 @@ class Dom {
 	x: 0
 	y: 0 
 	cbDefault: [0.23 1  0.32 1]*/
-	constructor(dom) {
+	constructor(wrapper, container) {
 		this.x = 0;
 		this.y = 0;
 		this.cbDefault = [0.23, 1, 0.32, 1]; 
 		this.duration = 2.5
-		this.dom = typeof dom == 'object' ? dom : document.querySelector(dom);
+		this.container = typeof container == 'object' ? container : document.querySelector(container);
+		this.wrapper = typeof wrapper == 'object' ? wrapper : document.querySelector(wrapper);
+		let containerRect = this.container.getClientRects()[0]
+		let wrapperRect = this.wrapper.getClientRects()[0]
+		this.maxMoveY = wrapperRect.height - containerRect.height;
+		console.log(containerRect, wrapperRect )
 	}
-	moveTo({x, y}) {
+	moveTo({x, y}, delay) {
 		if(x==0 && y==0) return false;
 		this.x += x;
 		this.y += y;
+		this.y = this.y > 0 ? 0: this.y,
+		this.y = this.y < this.maxMoveY ? this.maxMoveY : this.y
 		let style = [
 			`transition-timing-function: cubic-bezier(${this.cbDefault.join(',')})`,
-			`transition-duration: ${this.duration}s`,
+			`transition-duration: ${delay || this.duration}s`,
 			`transform: translate(${this.x}px, ${this.y}px)  scale(1) translateZ(0px)`
 		]
-		this.dom.style.cssText = style.join(';');
+		this.container.style.cssText = style.join(';');
 		
 		
 		/* this.dom.style.transitionTimingFunction = cube-bezier(${this.cbDefault.join(',')});
@@ -57,16 +64,17 @@ class MyScroll {
 		this.startY = 0
 		this.startTime =  0
 		this.throttle = throttle(this.touchmove);
-	    this.dom = new Dom(options.el);
+	    this.dom = new Dom(options.wrapper, options.container);
 		this.direction = (options.direction || this.direction).toLocaleLowerCase();
 		
 		this.bind()
 	}
 	bind() {
-		let dom = this.dom.dom,
+		let dom = this.dom.container,
 			that = this;
 		dom.addEventListener('touchstart', this.touchstart.bind(that), false);
 		dom.addEventListener('touchmove', this.throttle.bind(that), false);
+		dom.addEventListener('touchend', this.touchend.bind(that), false);
 	}
 	setPosition(e) { 
 		let touchEvent = e.changedTouches[0];
@@ -76,6 +84,7 @@ class MyScroll {
 	}
 	touchstart(e) {
 		this.setPosition(e);
+		e.preventDefault();
 	}
 	touchmove(e){
 		let touchEvent = e.changedTouches[0];
@@ -92,7 +101,29 @@ class MyScroll {
 			params.y = distance.y
 		if(this.direction.indexOf('x')!=-1)
 			params.x = distance.x
-		this.dom.moveTo(params);
+		this.dom.moveTo(params, 0.5);
 		this.setPosition(e);
+		e.preventDefault();
+	}
+	touchend(e){
+		let touchEvent = e.changedTouches[0];
+		let eX = touchEvent.pageX;
+		let eY = touchEvent.pageY;
+		let eTime = +new Date();
+		let distance = {
+			x: eX - this.startX,
+			y: eY - this.startY,
+			time: eTime - this.startTime
+		}
+		let params = {x:0, y:0}, forceX=0, forceY = 0;
+		if(this.direction.indexOf('y')!=-1) {
+			forceY = distance.y / distance.time
+			params.y = (forceY>0?1:-1)*(Math.pow(2, Math.abs(forceY)))
+		}
+		if(this.direction.indexOf('x')!=-1) {
+			forceX = distance.x / distance.time
+			params.x = (forceX>0?1:-1)*(Math.pow(5, Math.abs(forceX)))
+		}
+		this.dom.moveTo(params, 0);
 	}
 }
